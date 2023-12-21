e05ed7b1f1f4da011dade046714d07035796fc01
nndakurnia <nandaka38@gmail.com>
2023-12-21 10:16:58 +0700

    change serviceAccount

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,13 +1,9 @@
 import admin from 'firebase-admin'
 import { type ServiceAccount } from 'firebase-admin/app'
-import dotenv from 'dotenv'
-dotenv.config()
-
-// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-const serviceAccount: ServiceAccount = JSON.parse(process.env.SECRET_ACCOUNT_KEY!)
+import * as serviceAccount from './ServiceAccountKey.json'
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert(serviceAccount as ServiceAccount)
 })
 
 export default admin
